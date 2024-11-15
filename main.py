e1219d5433ac170f64a66566ca33bf8b11fcbb53
EuanRobertson1 <92741298+EuanRobertson1@users.noreply.github.com>
2024-11-15 16:20:25 +0000

    changes

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ def main():
     #get letter values
     values = getValues()
 
-    print(values)
-
     #define array abbr and call function to generate abbreviation 
     abbr = [] 
     abbr = generateAbbr(values)
+
+    
+
 
 
 #function for getting values. Creates dictionary
@@ -50,10 +51,12 @@ def generateAbbr(v):
     with open(f"{fileName}.txt", "r") as file:
         #create array containing each name in file
         names = [line.strip() for line in file]
+        print(f"names in file: {names}")
     #iterate each name
     for name in names:
         #split name so we can check each word in the name
         words = name.split()
+        print(f"current name: {words}")
         #this dictionary contains the current word alongside the score for each of its letters
         word_scores = {}
 
@@ -66,18 +69,22 @@ def generateAbbr(v):
 
             #check each letter and update scores 
             for i in range(length):
+                position_value = 0
                 char = word[i].upper()
-
+                print("current letter: " + char)
                 #If the letter is the first letter
                 if i == 0:
                     scores.append(0)
+                    
 
                 #If the letter is the last letter
                 elif i == length - 1:
                     if char == 'E':
                         scores.append(20)
+                        
                     else:
                         scores.append(5)
+                        
 
                 #If letter isnt first or last
                 else:
@@ -86,18 +93,21 @@ def generateAbbr(v):
                         position_value = 1
                     elif i == 2:
                         position_value = 2
-                    else:
+                    elif i > 2:
                         position_value = 3
-                
-                #obtain frequecy value of letter
-                letterFrequencyScore = v.get(char,0)
-                #calculate total score
-                totalScore = position_value + letterFrequencyScore
-                #add this to scores
-                scores.append(totalScore)
-            
+                    
+                    #obtain frequecy value of letter
+                    letterFrequencyScore = v.get(char,0)
+                    print(f"Letter frequency score:  {letterFrequencyScore}")
+                    #calculate total score
+                    totalScore = position_value + letterFrequencyScore
+                    #add this to scores
+                    scores.append(totalScore)
+                    
+            print(f"letter scores: {scores}")
             #update word scores
             word_scores[word] = scores
+            print(f"word score: {word_scores}")
 
         
         letters = []
@@ -120,16 +130,22 @@ def generateAbbr(v):
                 for k in range(j + 1, len(letters)):
                     #create a three letter abbreviation using i,j,k
                     abbreviation = letters[i] + letters[j] + letters[k]
-                    #caluculate total score using letter_scores dictionary from earlier
-                    totalScore = (letter_scores.get(letters[i], 0) +
-                                letter_scores.get(letters[j], 0) +
-                                letter_scores.get(letters[k], 0))
+                    print(f"abbreviation: {abbreviation}")
+                    #caluculate total score using v dictionary from earlier
+                    totalAScore = (word_scores.get(letters[i], 0) +
+                                  word_scores.get(letters[j], 0) +
+                                  word_scores.get(letters[k], 0))
                     #update best score and best abbriviation if neccessary
-                    if totalScore < bestScore:
-                        bestScore = totalScore
+                    print(f"This abbr has a score of: {totalAScore}")
+                    print(f"Current Best abbr {bestAbbreviation}")
+                    if totalAScore < bestScore:
+                        bestScore = totalAScore
+                        print("updating best abbr")
                         bestAbbreviation = abbreviation
 
-        #add to abbreviations array 
+        #add to abbreviations array
+        print(bestAbbreviation)
+        allAbbreviations = [] 
         allAbbreviations.append((name, bestAbbreviation))           
     
     
